04db4bdf57bb3c7952b0ebfc6006362ad45772f5
Penjat <theSpencerSymington@Gmail.com>
2019-01-21 13:28:54 -0800

    part 1 complete

--- ScrollViewImageGallery/ViewController.h
+++ ScrollViewImageGallery/ViewController.h
@@ -8,7 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 
 @end
--- ScrollViewImageGallery/ViewController.m
+++ ScrollViewImageGallery/ViewController.m
@@ -17,6 +17,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.scrollView.delegate = self;
+    self.scrollView.pagingEnabled = YES;
+    NSLog(@"creating images");
+    //-------------image 1------------------------
+    UIImageView *image1 = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"reboot"]];
+    
+    [self.scrollView addSubview:image1];
+    
+    image1.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    image1.contentMode = UIViewContentModeScaleAspectFill;
+    image1.clipsToBounds = YES;
+    
+    //-------------image 2------------------------
+    UIImageView *image2 = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"freakazoid"]];
+    
+    [self.scrollView addSubview:image2];
+    
+    image2.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    image2.contentMode = UIViewContentModeScaleAspectFill;
+    image2.clipsToBounds = YES;
+    
+    //-------------set layout------------------------
+    
+    
+    [image1.topAnchor constraintEqualToAnchor:self.scrollView.topAnchor].active = YES;
+    [image1.bottomAnchor constraintEqualToAnchor:self.scrollView.bottomAnchor].active = YES;
+    [image2.topAnchor constraintEqualToAnchor:self.scrollView.topAnchor].active = YES;
+    [image2.bottomAnchor constraintEqualToAnchor:self.scrollView.bottomAnchor].active = YES;
+    
+    [image1.leadingAnchor constraintEqualToAnchor:self.scrollView.leadingAnchor].active = YES;
+    [image2.trailingAnchor constraintEqualToAnchor:self.scrollView.trailingAnchor].active = YES;
+    
+    // width and height
+    [image1.heightAnchor constraintEqualToAnchor:self.view.heightAnchor].active = YES;
+    [image2.heightAnchor constraintEqualToAnchor:self.view.heightAnchor].active = YES;
+    
+    [image1.widthAnchor constraintEqualToAnchor:self.view.widthAnchor].active = YES;
+    [image2.widthAnchor constraintEqualToAnchor:self.view.widthAnchor].active = YES;
+    
+    [image1.trailingAnchor constraintEqualToAnchor:image2.leadingAnchor].active = YES;
+    
 }
 
 
