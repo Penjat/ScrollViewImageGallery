6bbabb4058281af0a0ba95d6746f3c6639cfbb46
Penjat <theSpencerSymington@Gmail.com>
2019-01-21 12:53:09 -0800

    Initial Commit

---
+++ ScrollViewImageGallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewImageGallery
+//
+//  Created by Spencer Symington on 2019-01-21.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
